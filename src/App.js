0561c26b7edc5ff5969a8eb03043d2d28ba03b51
Pavan kumar <pavankumar98989898@gmail.com>
2024-05-04 01:03:24 +0530

    The subproject's commit cd04b3bf17e7042dadac42718c23f4f587a427da has local modifications, now labeled as cd0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+
+function CheckBoxRunner(){
+  const [isChecked,changeState] = useState(false);
+  const [count,totalCount] = useState(0)
+  let message;
+  let countMsg 
+  const CheckedBox=()=>{ 
+    changeState(!isChecked);
+
+    totalCount(count+1)
+  }
+  if(isChecked&& count <= 10){
+    message = `Hey Button is Checked!!!!!`;
+    
+    countMsg = `Your Are On The Right Track`;
+  }
+  else if(isChecked && count > 21){
+    message = `You got Some Penis`;
+    countMsg = `You can Fuck this many Chicks ${count}`
+  }
+  else{
+    message = `Hey Button is Not Checked!!!`;
+    countMsg = `You Got some bills to Pay You Know!!!`
+  }
+  return(
+
+    <>
+    <lable>Please Check to show the message</lable>
+    <input type='checkBox' checked = {isChecked} onChange={CheckedBox}/>
+    <p>{count}</p>
+    <p>{countMsg }</p>
+    <p>{message}</p>
+    </>
+
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CheckBoxRunner/>
   );
 }
 
